b746c532598942db2a553b52197d772fcaa9265d
sessi.hounnou@epitech.eu <cdric.hounnou2@gmail.com>
2022-06-08 17:48:39 -0400

    new feature

--- src/components/dragableComponent.vue
+++ src/components/dragableComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <header-surver />
-  <div class="my-10 sm:w-[20%] py-8 px-8 border bg-white">
+  <div class="my-16 sm:w-[20%] py-8 px-8 border bg-white">
     <div class="h-3/4 flex flex-col justify-around text-gray-500">
       <h3
         class="select-none my-4 py-4 border pl-1 text-sm flex items-center py-2 mb-2 hover:bg-gray-100 hover:text-gray-700 transition duration-200 ease-in"
       >
         <img src="../assets/drag.svg" class="mx-4" alt="drag" />
         <a
-          class="mx-8 hover:text-black transition duration-200 ease-linear text-blue-500"
+          class="mx-8 hover:text-black transition duration-200 ease-linear"
           href="#"
           >Home</a
         >
