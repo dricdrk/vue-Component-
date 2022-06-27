76e7c8d8f8b8dc077ff16038ef336a18f3808711
dricdrk <cdric.hounnou2@gmail.com>
2022-06-27 14:23:41 -0400

    implement dependancy to parse csv coe

---
+++ src/components/importcsv.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>A completely un-styled demo.</div>
+  <br />
+  <a href="https://storage.jgile.com/csv-sample.csv">Download Sample CSV</a>
+  <br /><br />
+  <vue-csv-import v-model="mappedCsv" :fields="fields">
+    <vue-csv-toggle-headers></vue-csv-toggle-headers>
+    <vue-csv-errors></vue-csv-errors>
+    <div>
+      <vue-csv-input></vue-csv-input>
+    </div>
+    <vue-csv-map></vue-csv-map>
+  </vue-csv-import>
+  <br /><br />
+  <div>
+    <pre>{{ mappedCsv }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      mappedCsv: null,
+      fields: {
+        name: { required: false, label: "Name" },
+        age: { required: true, label: "Age" },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import dragableComponent from "../components/dragableComponent.vue";
+import import_csv from "../components/importcsv.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: dragableComponent,
+    },
+    {
+      path: "/import",
+      name: "import",
+      component: import_csv,
     },
     // {
     //   path: "/about",
